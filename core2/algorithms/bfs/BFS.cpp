9a23ed42a6a111976d9ae4adae71362c6ff43298
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-02-07 17:14:24 +0100

    Update BFS.cpp

--- core2/algorithms/bfs/BFS.cpp
+++ core2/algorithms/bfs/BFS.cpp
@@ -1,100 +1,76 @@
-******************************************************************************
+/******************************************************************************
 
-                              BFS.
-
+                              BFS
 
 *******************************************************************************/
 
-
 #include <iostream>
-#include <list>
+#include <vector>
 #include <queue>
 using namespace std;
 
 
-
-
-class Graph{
-private:
-    int v; // number of vertices
-    list <int> adj[10]; // Array of lists
-   
-   
+class GRAPH{
+    
 public:
-    // constructor
-    Graph(int v){
-        // stores numebr of passed vertices to the constructor
-        // avoidng name conflict
-        this->v=v;
+    int V;
+    vector<vector<int>> adj;
+    
+    GRAPH(int vertices){
+        V = vertices;
+        adj.resize(V);
     }
-   
-   
-    // Add direted edge : one direction connection
-    void addEdge(int u , int w){
-       
-        if (u>=0 && u<v  && w >=0 && w<v){
-            adj[u].push_back(w);   // Adds w to the neighbor of u
-        } else{
-           
-            cout << "Invalid edge!" << endl;
-        }
-       
-    void BFS_TRAVERSE(int start_point){
-       
+    
+    
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
-        // q: queue of visited nodes
-        bool visited_vertices[10]={false};
-        queue <int> q;
-        visited_vertices[start_point] = true;
-        q.push(start_point);
-       
-        cout << "Starting vertice is:" << start_point << endl;
-       
-        while(!q.empty()){
-           
-            int node = q.front(); // first node  
-            cout << node << "  ";
-            q.pop(); // move to next node  
-           
-            for(int neighbor:adj[node]){
-                if(!visited_vertices[neighbor]){
-                    visited_vertices[neighbor]=true;
-                    q.push(neighbor)
+    
+    
+    void BFS(int start){
+        
+        queue<int> q;
+        vector<bool> visited(V, false);
+        
+        
+        visited[start]=true;
+        q.push(start);
+        
+        while( !q.empty() ){
+            
+            int node = q.front();
+            q.pop();
+            cout << node << " " << endl;
+            
+            for(int i=0; i<adj[node].size() ;i++){
+                int neighbor = adj[node][i];
+                if (!visited[neighbor]){
+                    visited[neighbor]=true;
+                    q.push(neighbor);
                 }
             }
+            
         }
-       
-        cout << endl;
+  
     }
-       
-       
-       
-       
+    
+    
+    
+}; // end 
 
-    }
-   
-   
+int main() {
+    GRAPH g(6);
 
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
 
+    cout << "BFS starting from node 0: ";
+    g.BFS(0);
 
-
-int main()
-{  
-    int vertice = 5;
-    // Graph edges: (0,1) ,(0,2), (0,3), (0,4) , (1,2). (3,3)
-    Graph object(vertice);
-    object.addEdge(0,1); // edge between 0 and 1
-    object.addEdge(0,2); // edge between 0 and 1
-    object.addEdge(0,3); // edge between 0 and 1
-    object.addEdge(0,4); // edge between 0 and 1
-    object.addEdge(1,2); // edge between 0 and 1
-    object.addEdge(3,3); // edge between 0 and 1
-   
-    cout << "Determine starting point of traverse"<<endl;
-    int start_point;
-    cin>>start_point;
-   
-    object.BFS_TRAVERSE(start_point);
-
-
+    return 0;
 }
