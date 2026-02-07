8929af067df7c2c41feb06dd73d4266eb890d4c3
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-02-07 17:24:02 +0100

    Update main.cpp

--- core4/leetcode/Arrays/P733/main.cpp
+++ core4/leetcode/Arrays/P733/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
-class Solution {
+class Solution_Recursion {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
@@ -43,6 +44,48 @@ public:
     }
 };
 
+
+class Solution_BFS {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int rows = image.size();
+        int cols = image[0].size();
+
+        int originalColor = image[sr][sc];
+        if (originalColor == color)
+            return image;  // Nothing to change
+
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+
+        // Direction vectors: Up, Down, Left, Right
+        int dr[4] = {-1, 1, 0, 0};
+        int dc[4] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
+                    image[nr][nc] == originalColor) {
+
+                    image[nr][nc] = color;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        return image;
+    }
+};
+
+
+
 int main() {
     vector<vector<int>> image = {
         {1,1,1},
@@ -52,7 +95,7 @@ int main() {
 
     int sr = 1, sc = 1, newColor = 2;
 
-    Solution sol;
+    Solution_Recursion sol;
     vector<vector<int>> res = sol.floodFill(image, sr, sc, newColor);
 
     cout << "Flood Filled Image:\n";
@@ -64,5 +107,6 @@ int main() {
 
     return 0;
 }
+
 
 
