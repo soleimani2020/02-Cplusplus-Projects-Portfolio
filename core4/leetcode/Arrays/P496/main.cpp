19a72f64241238dad812e68a5057871f314f21c9
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-02-11 09:21:03 +0100

    Update main.cpp

--- core4/leetcode/Arrays/P496/main.cpp
+++ core4/leetcode/Arrays/P496/main.cpp
@@ -38,11 +38,50 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    
+    unordered_map<int,int> mp; // nums2[i]--->nextGreaterElement 
+    stack<int> s;
+    
+    
+    for(int i=nums2.size()-1 ; i>=0 ;i--){
+        
+        // Removing irrelavat elements form the stack
+        while(!s.empty() && s.top()<= nums2[i]){
+            s.pop();
+        }
+        
+        
+        if(s.empty()){
+            mp[nums2[i]]=-1;
+        }else{
+            mp[nums2[i]]=s.top();
+        }
+        
+        s.push(nums2[i]);
+    }
+    
+    
+    vector<int> ans;
+    
+    for(int i=0; i<nums1.size();i++){
+        ans.push_back(mp[nums1[i]]);
+    }
+    
+    return ans;
+ 
+    }
+
+};
+
 int main() {
     vector<int> nums1 = {4,1,2};
     vector<int> nums2 = {1,3,4,2};
 
-    Solution_BF sol;
+    Solution sol;
     vector<int> R = sol.nextGreaterElement(nums1, nums2);
 
     for (int num : R) {
@@ -51,3 +90,6 @@ int main() {
 
     return 0;
 }
+
+// ans : {-1,3,-1}
+
