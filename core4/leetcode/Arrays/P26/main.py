f06a4943040daa2ad59759cb3d6e87f1364c3c8d
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-02-15 18:07:23 +0100

    Add files via upload

---
+++ core4/leetcode/Arrays/P26/main.py
@@ -0,0 +1,39 @@
+class Solution:
+    
+    def RemoveDuplicate(self, nums):
+        
+        if not nums:
+            return  0;
+            
+        replace = 1;
+        
+        for i in range(1,len(nums)):
+            if nums[i-1] != nums[i]:
+                nums[replace] = nums[i];
+                replace+=1 
+        
+        return replace
+    
+    
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    nums = [5,6,7,7,7,8]
+    
+    solve = Solution()
+    
+    R = solve.RemoveDuplicate(nums)
+    
+    print(f"Number of unique elements is: {R}")
+    print("New array:", nums[:R])
+    
+
+
