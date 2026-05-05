27e19ec8c86937f47275e4d4f154944c138cb018
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-05-05 19:58:50 +0200

    Update main.cpp

--- core4/leetcode/Arrays/P575/main.cpp
+++ core4/leetcode/Arrays/P575/main.cpp
@@ -33,38 +33,6 @@ public:
 };
 
 
-class Solution {
-public:
-    int distributeCandies(vector<int>& num) {
-        
-        int n = num.size();
-        vector<int> arr(n,1);
-        
-        // Scan from left to right 
-        for(int i=1; i < n; i++){
-            if(num[i] > num[i-1]){
-                arr[i]=arr[i-1]+1;
-            }
-        }
-        
-        
-        // Scan from right to left 
-        for(int i=n-2; i>=0;i--){
-            if(num[i] > num[i+1]){
-                arr[i] = max(arr[i],arr[i+1]+1);
-            }
-        }
-        
-        int sum=0;
-        for(int num : arr){
-            sum+=num;
-        }
-        
-        return sum;
-        
-    }
-};
-
 
 
 
