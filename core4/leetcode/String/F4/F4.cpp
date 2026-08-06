e3cd4f3e7d0b047665dfd2f480c9a1455e668bbd
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-06 10:55:24 +0100

    Update F4.cpp

--- core4/leetcode/String/F4/F4.cpp
+++ core4/leetcode/String/F4/F4.cpp
@@ -1,1 +1,43 @@
 
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        int n = s.size();
+        string ans = "";
+
+        for(int i = 0; i < n; i++) {
+
+            // Extract individual word
+            string word = "";
+
+            while(i < n && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+           
+
+            // Reverse the word using two pointers instead of reverse(word.begin(),word.end())
+            int left = 0;
+            int right = word.size() - 1;
+
+            while(left < right) {
+                swap(word[left], word[right]);
+                left++;
+                right--;
+            }
+
+            // Add reversed word to answer
+            if(!word.empty()) {
+
+                if(!ans.empty()) {
+                    ans += ' ';
+                }
+
+                ans += word;
+            }
+        }
+
+        return ans;
+    }
+};
