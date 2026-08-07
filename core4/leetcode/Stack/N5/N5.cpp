6d0b13f990f326f0eda16609291c953860984857
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-07 09:33:56 +0100

    Update N5.cpp

---
+++ core4/leetcode/Stack/N5/N5.cpp
@@ -0,0 +1,40 @@
+#include <unordered_map>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int> mp; // nums2[i] -> next greater element
+        
+        int n = nums2.size();
+        stack<int> st;
+
+        for(int i = n-1; i >= 0; i--) {
+
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) {
+                mp[nums2[i]] = -1;
+            }
+            else {
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
