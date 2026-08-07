5e88fca74d9705aec99f0da70423aaa331c181e8
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-07 22:10:17 +0100

    Update N8.cpp

--- core4/leetcode/Stack/N8/N8.cpp
+++ core4/leetcode/Stack/N8/N8.cpp
@@ -1,46 +1,56 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> numsbers;
+        
+        // 12[a]
+        int n = s.size();
+        int num =0 ;
+        string inner_str = "";
+        
         stack<string> letters;
+        stack<int> numbers;
         
-        int num =0;
-        string ans = "";
-        
-        for(char c : s) {
+        for(char c: s){
             if(isdigit(c)){
-                num = num*10 + c - '0';
-            }else if(c != '[' && c != ']') {
-                ans += c;
+                num = num *10 + c - '0';
+            }else if(c != '[' && c != ']'){
+                inner_str += c;
             }
             
-            if(c=='['){
-                letters.push(ans);
-                numsbers.push(num);
-                ans = "";      // start a new substring
+            if(c =='['){
+                numbers.push(num);
+                letters.push(inner_str);
                 num = 0;
+                inner_str ="";
             }
             
-            if(c==']'){
-                int repeat = numsbers.top();
-                numsbers.pop();
-                string repeat_st = letters.top();
+            if(c == ']'){
+                int repeat = numbers.top();
+                numbers.pop();
+                string outer_str = letters.top();
                 letters.pop();
                 string str_new = "";
-                for(int i=0; i < repeat; i++){
-                    str_new += ans;
+                for(int i=0; i<repeat; i++){
+                    str_new += inner_str;
                 }
                 
-                str_new = repeat_st + str_new;
-                ans = str_new;
+                str_new = outer_str + str_new;
+                inner_str = str_new;
                 
                 
             }
-
+            
+            
+            
+            
+            
+            
         }
         
-        return ans;
-
+        return inner_str;
+        
+        
+        
         
     }
 };
