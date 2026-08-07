83b1b8423657d84aa6553fb6038fa1723a67017f
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-07 18:11:22 +0100

    Update N8.cpp

---
+++ core4/leetcode/Stack/N8/N8.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> numsbers;
+        stack<string> letters;
+        
+        int num =0;
+        string ans = "";
+        
+        for(char c : s) {
+            if(isdigit(c)){
+                num = num*10 + c - '0';
+            }else if(c != '[' && c != ']') {
+                ans += c;
+            }
+            
+            if(c=='['){
+                letters.push(ans);
+                numsbers.push(num);
+                num = 0;
+            }
+            
+            if(c==']'){
+                int repeat = numsbers.top();
+                numsbers.pop();
+                string repeat_st = letters.top();
+                letters.pop();
+                string str_new = "";
+                for(int i=0; i < repeat; i++){
+                    str_new += ans;
+                }
+                
+                str_new = repeat_st + str_new;
+                ans = str_new;
+                
+                
+            }
+
+        }
+        
+        return ans;
+
+        
+    }
+};
