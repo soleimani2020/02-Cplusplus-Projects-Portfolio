f790e925873a20c5f8d53d99de536fb6dbf8c5f2
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-07 10:14:03 +0100

    Update N4.cpp

--- core4/leetcode/Stack/N4/N4.cpp
+++ core4/leetcode/Stack/N4/N4.cpp
@@ -1,42 +1,42 @@
+#include <vector>
 #include <stack>
+
+using namespace std;
 
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& nums) {
-        
-        
+
         int n = nums.size();
+
+        // Stores the number of days until a warmer temperature
         vector<int> ans(n, 0);
-        stack<int> st;  // stores indices
-        
-        
-        for(int i=n-1; i>=0 ; i--){
-            
-            int x = nums[i];
-            
-            
-            // remove useless warmer days
-            while(! st.empty() && nums[st.top()] <= x){
+
+        // Monotonic decreasing stack storing indices
+        stack<int> st;
+
+        // Traverse from right to left
+        for(int i = n - 1; i >= 0; i--) {
+
+            // Remove indices whose temperatures are smaller
+            // or equal because they cannot be the next warmer day
+            while(!st.empty() && nums[st.top()] <= nums[i]) {
                 st.pop();
             }
-            
-            // if stack not empty, top is next warmer day
-            if (!st.empty()) {
+
+            // If stack is empty, no warmer temperature exists
+            if(st.empty()) {
+                ans[i] = 0;
+            }
+            else {
+                // Distance between current day and warmer day
                 ans[i] = st.top() - i;
             }
-            
-            st.push(i);
 
-            
-            
-            
+            // Store current index for future comparisons
+            st.push(i);
         }
-        
+
         return ans;
-        
-        
-        
-        
-        
     }
 };
