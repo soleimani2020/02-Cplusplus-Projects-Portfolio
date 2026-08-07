ba7204af5768e1a3c2c16190b7eeb8d52be0b39f
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-07 12:14:46 +0100

    Update N6.cpp

--- core4/leetcode/Stack/N6/N6.cpp
+++ core4/leetcode/Stack/N6/N6.cpp
@@ -1,35 +1,60 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+
         int n = heights.size();
 
         vector<int> left(n), right(n);
+
         stack<int> st;
 
         // Previous Smaller Element
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
+        for(int i = 0; i < n; i++) {
 
-            left[i] = st.empty() ? -1 : st.top();
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) {
+                left[i] = 0;
+            }
+            else {
+                left[i] = st.top() + 1;
+            }
+
             st.push(i);
         }
 
-        while (!st.empty()) st.pop();
+
+        while(!st.empty()) {
+            st.pop();
+        }
+
 
         // Next Smaller Element
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
+        for(int i = n-1; i >= 0; i--) {
 
-            right[i] = st.empty() ? n : st.top();
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) {
+                right[i] = n-1;
+            }
+            else {
+                right[i] = st.top() - 1;
+            }
+
             st.push(i);
         }
+
 
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            int width = right[i] - left[i] - 1;
+        for(int i = 0; i < n; i++) {
+
+            int width = right[i] - left[i] + 1;
+
             ans = max(ans, heights[i] * width);
         }
 
