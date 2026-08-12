7ed6dbcf41e2e057066f06b5e55f589837b78b5d
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-08-12 17:13:35 +0100

    Update N12.cpp

--- core4/leetcode/Stack/N12/N12.cpp
+++ core4/leetcode/Stack/N12/N12.cpp
@@ -1,35 +1,48 @@
 class MyStack {
 public:
 
+
     queue<int> q;
 
-    MyStack() {
-    }
 
+    MyStack() {
+        
+    }
+    
     void push(int x) {
         q.push(x);
-
         int n = q.size();
-
-        for (int i = 1; i < n; i++) {
+        for(int i=1; i < n; i++){
             int value = q.front();
             q.pop();
             q.push(value);
         }
+        
     }
-
+    
     int pop() {
         int value = q.front();
         q.pop();
-
         return value;
+        
     }
-
+    
     int top() {
         return q.front();
+        
     }
-
+    
     bool empty() {
         return q.empty();
+        
     }
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
